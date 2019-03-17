6c218198ceaaf462ccc99f05c4ec6d0ae2f98fd0
xzw <2311075864@qq.com>
2019-03-17 10:16:01 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+      <left></left>
+      <div class="main">
+         <router-view></router-view>
+      </div>
+  </div>
+</template>
+
+<script>
+import left from '@/components/common/left'
+import pos from  '@/components/page/pos'
+export default {
+  name: 'App',
+  components:{
+   left
+  }
+}
+</script>
+
+<style>
+/* #app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 0px;
+} */
+.main{
+  float: left;
+  width: 95%;
+  background-color: #e8f2f7;
+  height: 100%;
+  overflow: auto;
+}
+</style>
---
+++ src/components/common/left.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="left-n">
+        <ul>
+            <li>
+                <i class="icon iconfont icon-goumai"></i>
+            <div>收银</div>
+            </li>
+            <li>
+                <i class="icon iconfont icon-shop"></i>
+                <div>店铺</div>
+            </li>
+            <li>
+                <i class="icon iconfont icon-hanbao"></i>
+                <div>商品</div>
+            </li>
+            <li>
+            <i class="icon iconfont icon-huiyuanqia"></i>
+               <div>会员</div>
+            </li>
+             <li>
+            <i class="icon iconfont icon-tongji"></i>
+               <div>统计</div>
+            </li>
+             <li>
+            <i class="icon iconfont icon-shezhi"></i>
+               <div>设置</div>
+            </li>
+        </ul>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="">
+    .left-n{
+       color:#fff;
+       font-size:10px;
+       height:100%; 
+       background-color: #1D8ce0;
+       float:left;
+       width:5%;
+    }
+    .iconfont{
+       font-size:24px;
+    }
+    .left-n ul{
+        padding:0px;
+        margin: 0px;
+    }
+    .left-n li{
+         list-style: none; 
+        text-align: center;
+        border-bottom:1px solid #20a0ff;
+        padding:10px;
+    }
+</style>
